aa8840599ee894b62522b29c9602fddf3d28285b
Camilo <137337272+caamilo03@users.noreply.github.com>
2026-04-16 18:55:19 -0500

    fix: delete code of batch service

--- src/main/java/co/edu/udea/agrocore/backend/domain/port/in/CreateCropBatchUseCase.java
+++
@@ -1,8 +0,0 @@
-package co.edu.udea.agrocore.backend.domain.port.in;
-
-import co.edu.udea.agrocore.backend.domain.model.CropBatch;
-
-public interface CreateCropBatchUseCase {
-    // Recibe un lote sin ID, le aplica reglas de negocio, y lo devuelve ya creado
-    CropBatch create(CropBatch cropBatch);
-}
--- src/main/java/co/edu/udea/agrocore/backend/domain/port/in/GetCropBatchUseCase.java
+++
@@ -1,8 +0,0 @@
-package co.edu.udea.agrocore.backend.domain.port.in;
-
-import co.edu.udea.agrocore.backend.domain.model.CropBatch;
-import java.util.List;
-
-public interface GetCropBatchUseCase {
-    List<CropBatch> getAll();
-}
